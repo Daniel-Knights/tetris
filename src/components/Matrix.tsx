02a155aee8d3bdd5e6a037ea2bbd34d2cce0ef67
Dan Knights <daniel.knights@28b.co.uk>
2024-04-01 16:53:31 +0100

    perf(matrix): use canvas

--- src/components/Matrix.tsx
+++ src/components/Matrix.tsx
@@ -1,4 +1,9 @@
+import { useEffect, useMemo, useRef } from "react";
+
 import { Coord } from "../modules";
+
+const CELL_SIZE = 20;
+const GAP_SIZE = 4;
 
 function Matrix({
   dimensions,
@@ -14,10 +19,18 @@ function Matrix({
   highlightedCoords?: Coord[];
   outlinedCoords?: Coord[];
 }): JSX.Element {
-  const bgOpacity = bg ? "opacity-10" : "opacity-0";
+  const computedBodyStyle = window.getComputedStyle(document.body);
+  const colorPrimary = computedBodyStyle.getPropertyValue("--color-primary");
+  const colorSecondary = computedBodyStyle.getPropertyValue("--color-secondary");
+  const bgOpacity = bg ? 0.1 : 0;
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   // If dimensions aren't passed, fit matrix to highlighted coords
-  const normalisedCoords: Coord[] = dimensions === undefined ? [] : highlightedCoords;
+  const normalisedCoords: Coord[] = useMemo(
+    () => (dimensions === undefined ? [] : highlightedCoords),
+    [dimensions, highlightedCoords]
+  );
 
   let adjustedColumns = dimensions?.columns ?? -1;
   let adjustedRows = dimensions?.rows ?? -1;
@@ -40,42 +53,79 @@ function Matrix({
     adjustedRows += hasMinusY ? 2 : 1;
   }
 
-  return (
-    <div
-      className="grid gap-1"
-      style={{
-        gridTemplateRows: `repeat(${adjustedRows},20px)`,
-        gridTemplateColumns: `repeat(${adjustedColumns},20px)`,
-      }}
-    >
-      {Array.from({ length: adjustedRows * adjustedColumns }, (_, i) => {
-        const coord = Coord.fromIndex(i, {
+  // Paint cells
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+
+    ctx.save();
+    ctx.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
+    // Scale for retina displays
+    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    ctx.translate(GAP_SIZE / 2, GAP_SIZE / 2);
+
+    for (let row = 0; row < adjustedRows; row += 1) {
+      for (let col = 0; col < adjustedColumns; col += 1) {
+        const x = col * (CELL_SIZE + GAP_SIZE);
+        const y = row * (CELL_SIZE + GAP_SIZE);
+
+        const coord = Coord.fromIndex(row * adjustedColumns + col, {
           rows: adjustedRows,
           columns: adjustedColumns,
         });
 
-        return (
-          <svg
-            key={window.crypto.randomUUID()}
-            className={
-              coord.isIn([...normalisedCoords, ...outlinedCoords]) ? "" : bgOpacity
-            }
-            viewBox="0 0 20 20"
-            height="20"
-            width="20"
-          >
-            <path
-              stroke="rgb(var(--color-primary))"
-              fill="rgb(var(--color-secondary))"
-              d="M.701.701H19.3V19.3H.7z"
-            />
-            {(!coord.isIn(outlinedCoords) || coord.isIn(normalisedCoords)) && (
-              <path fill="rgb(var(--color-primary))" d="M5.35 5.35h9.3v9.3h-9.3z" />
-            )}
-          </svg>
-        );
-      })}
-    </div>
+        ctx.globalAlpha = coord.isIn([...normalisedCoords, ...outlinedCoords])
+          ? 1
+          : bgOpacity;
+        ctx.fillStyle = `rgb(${colorSecondary})`;
+        ctx.strokeStyle = `rgb(${colorPrimary})`;
+        ctx.lineWidth = 1;
+
+        ctx.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+        ctx.strokeRect(x, y, CELL_SIZE, CELL_SIZE);
+
+        // Center square
+        if (!coord.isIn(outlinedCoords) || coord.isIn(normalisedCoords)) {
+          const centerSquareDimensions = CELL_SIZE / 2;
+
+          ctx.fillStyle = `rgb(${colorPrimary})`;
+          ctx.fillRect(
+            x + centerSquareDimensions / 2,
+            y + centerSquareDimensions / 2,
+            centerSquareDimensions,
+            centerSquareDimensions
+          );
+        }
+      }
+    }
+
+    return () => {
+      ctx.restore();
+    };
+  }, [
+    adjustedColumns,
+    adjustedRows,
+    bgOpacity,
+    colorPrimary,
+    colorSecondary,
+    normalisedCoords,
+    outlinedCoords,
+  ]);
+
+  const canvasHeight = adjustedRows * (CELL_SIZE + GAP_SIZE);
+  const canvasWidth = adjustedColumns * (CELL_SIZE + GAP_SIZE);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      // Scale for retina displays
+      height={Math.floor(canvasHeight * window.devicePixelRatio)}
+      width={Math.floor(canvasWidth * window.devicePixelRatio)}
+      style={{
+        height: `${canvasHeight}px`,
+        width: `${canvasWidth}px`,
+      }}
+    />
   );
 }
 
