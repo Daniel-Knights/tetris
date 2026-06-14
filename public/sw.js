dbbe52caa84ae611bea2ba02b473e4ddc6261f58
Daniel Knights <59598622+Daniel-Knights@users.noreply.github.com>
2026-06-14 22:11:15 +0100

    perf(pwa): stale-while-revalidate and ignoreVary

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,14 @@
 /*
   Some notes on this:
-  - Offline functionality doesn't work in dev, it can only be tested by running `pnpm run preview:web`
-  - The scope when registered must be the root. If you register it at a subpath, it will hang on 'trying to install'.
+  - Offline functionality doesn't work in dev, it can only be tested by running `pnpm run preview:web`.
+    This is because:
+    - PWAs require HTTPS, but HTTP is used in dev
+    - Vite ignores service workers entirely due to it conflicting with HMR
+  - The scope when registered must be the root. If you register it at a subpath,
+    it will hang on 'trying to install'.
   - The static resources are read from `dist-web` and injected by the build script
+  - This file may be stale in preview, as we don't use `self.skipWaiting()` inside the install handler.
+    `skipWaiting` can be triggered manually from 'Application' -> 'Service workers'.
 */
 
 const CACHE_NAME = "tetris-v1";
@@ -43,14 +49,16 @@ self.addEventListener("fetch", (ev) => {
   ev.respondWith(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(ev.request);
+      const cachedResponse = await cache.match(ev.request, {
+        // Ignore Vary header to avoid cache misses
+        ignoreVary: true,
+      });
 
       if (cachedResponse) {
-        try {
-          await cache.add(ev.request);
-        } catch {
-          // Offline
-        }
+        // Stale-while-revalidate to avoid blocking requests
+        cache.add(ev.request).catch(() => {
+          // noop
+        });
 
         return cachedResponse;
       }
